22234046360f46306abb0fc8408c20c1dd058dac
voltyea <volty764@gmail.com>
2025-08-03 20:49:54 +0530

    updated install.sh

--- install.sh
+++ install.sh
@@ -6,13 +6,13 @@ rfkill unblock bluetooth
 # adding chaotic-aur
 if [ ! -f /etc/pacman.d/chaotic-mirrorlist ]; then
 
-{
-sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
-sudo pacman-key --lsign-key 3056513887B78AEB
+  {
+    sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+    sudo pacman-key --lsign-key 3056513887B78AEB
 
-sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
-sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
-} || { exit 1; }
+    sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
+    sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+  } || { exit 1; }
 
   if ! grep -qF "[chaotic-aur]" /etc/pacman.conf; then
     echo -e "\n\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist" | sudo tee -a /etc/pacman.conf
@@ -43,8 +43,8 @@ if grep -q "^#[multilib]" /etc/pacman.conf; then
 fi
 if ! grep -q "^\[multilib\]" /etc/pacman.conf; then
   sudo tee -a /etc/pacman.conf >/dev/null <<EOF
-[multilib]
-Include = /etc/pacman.d/mirrorlist
+  [multilib]
+  Include = /etc/pacman.d/mirrorlist
 EOF
 fi
 sudo sed -i '/^\[multilib\]/,/^\[/{s/^#\(Include = \/etc\/pacman\.d\/mirrorlist\)/\1/}' /etc/pacman.conf
